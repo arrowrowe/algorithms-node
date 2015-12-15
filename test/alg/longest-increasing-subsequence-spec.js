e7b71d34728460787d88c445cdb7803cefeffb9b
arrowrowe <arrowrowe@gmail.com>
2015-12-15 22:50:01 +0800

    Typo: subsequence -> subsequences

--- test/alg/longest-increasing-subsequence-spec.js
+++ test/alg/longest-increasing-subsequence-spec.js
@@ -11,7 +11,7 @@ describe('The longest increasing subsequence problem', () => {
     T([0, 4, 2, 5, 3], 3);
     T([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15], 6);
   });
-  it('allows non-decreasing subsequence as well', () => {
+  it('allows non-decreasing subsequences as well', () => {
     T([0, 1, 4, 4, 4, 4, 2, 3], 6);
   });
 });
