98d4e313f6c6dacc48648dd06a290e752f2e140e
arrowrowe <arrowrowe@gmail.com>
2015-12-26 22:39:38 +0800

    Comment Dijkstra's algorithm

--- alg/dijkstra.js
+++ alg/dijkstra.js
@@ -2,6 +2,15 @@
 
 const newArray = require('../util/new-array');
 
+/*
+  Dijkstra's algorithm
+    https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+  Given
+    distanceMatrix[i][j] = distance between Node i and Node j, for every 0 <= i, j < count of all nodes;
+    initialIndex, from which node all shortest distances should be calculated.
+  Return
+    shortest distances from the initial node.
+ */
 module.exports = (distanceMatrix, initialIndex) => {
 
   initialIndex = initialIndex || 0;
@@ -16,12 +25,15 @@ module.exports = (distanceMatrix, initialIndex) => {
 
   const visit = currentIndex => {
     const currentDistance = distances[currentIndex];
+    // Mark current node as visited.
     visited[currentIndex] = true;
     unvisitedDistances[currentIndex] = Infinity;
+    // Loop over all UNVISITED neighbors of current node.
     distanceMatrix[currentIndex].forEach((distance, neighborIndex) => {
       if (visited[neighborIndex]) {
         return;
       }
+      // Update this neighbor's distance, if smaller found.
       unvisitedDistances[neighborIndex] = distances[neighborIndex] = Math.min(
         distances[neighborIndex],   // previously marked distance
         currentDistance + distance  // newly calculated distance
@@ -29,6 +41,7 @@ module.exports = (distanceMatrix, initialIndex) => {
     });
   };
 
+  // Visit the initial node first.
   let nextCurrentIndex = initialIndex;
   for(;;) {
     visit(nextCurrentIndex);
