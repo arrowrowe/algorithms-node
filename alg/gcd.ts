f7dbfbdcd6fe7fb9b19d2ddd1ba7a3efaca81656
arrowrowe <arrowrowe@gmail.com>
2016-03-26 13:01:50 +0800

    Better Readale GCD

--- alg/gcd.ts
+++ alg/gcd.ts
@@ -1,5 +1,14 @@
-const __gcd = (a: number, b: number): number => b % a ? _gcd(a, b - Math.floor(b / a) * a) : a;
+const __gcd = (a: number, b: number): number =>
+  b % a ?
+    _gcd(a, b - Math.floor(b / a) * a) :
+    a;
 
-const _gcd = (a: number, b: number): number => __gcd.apply(null, a < b ? [a, b] : [b, a]);
+const _gcd = (a: number, b: number): number =>
+  a < b ?
+    __gcd(a, b) :
+    __gcd(b, a);
 
-export default (a: number, b: number): number => a === 0 ? b : b === 0 ? a : _gcd(Math.abs(a), Math.abs(b));
+export default (a: number, b: number): number =>
+  a === 0 ? b :
+  b === 0 ? a :
+  _gcd(Math.abs(a), Math.abs(b));
